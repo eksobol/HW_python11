e46a73afbac4cef42a66bec725db2d82564f8016
Ekaterina Soboleva <ek.a.soboleva@ya.ru>
2024-10-28 23:46:55 +0300

    +allure

--- test/test_browser.py
+++ test/test_browser.py
@@ -1,22 +1,24 @@
 from pages.registration_pages import RegistrationStudent
+import allure
 
 
 def test_registration_student():
     registration_student = RegistrationStudent()
 
-    registration_student.open('https://demoqa.com/automation-practice-form')
-    registration_student.fill_first_name('Anna')
-    registration_student.fill_last_name('Ivanova')
-    registration_student.fill_email('abc@ya.ru')
-    registration_student.fill_gender()
-    registration_student.fill_number('8111111111')
-    registration_student.fill_date_of_birth('08', 7, 2000)
-    registration_student.fill_subject('Chemistry')
-    registration_student.fill_hobby()
-    registration_student.fill_image('image.png')
-    registration_student.fill_current_address('homeland')
-    registration_student.fill_state_and_city()
-    registration_student.fill_submit()
+    with allure.step('Открываем форму'):
+        registration_student.open('https://demoqa.com/automation-practice-form')
+        registration_student.fill_first_name('Anna')
+        registration_student.fill_last_name('Ivanova')
+        registration_student.fill_email('abc@ya.ru')
+        registration_student.fill_gender()
+        registration_student.fill_number('8111111111')
+        registration_student.fill_date_of_birth('08', 7, 2000)
+        registration_student.fill_subject('Chemistry')
+        registration_student.fill_hobby()
+        registration_student.fill_image('image.png')
+        registration_student.fill_current_address('homeland')
+        registration_student.fill_state_and_city()
+        registration_student.fill_submit()
 
     # проверки
 
