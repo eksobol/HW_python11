7a0fe08d91766fc797af51a8e8a1d686b8093da9
Ekaterina Soboleva <ek.a.soboleva@ya.ru>
2024-10-28 22:54:56 +0300

    +browser

--- test/conftest.py
+++ test/conftest.py
@@ -1,19 +1,31 @@
 import pytest
 from selene import browser
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-
 
 # добавляем фикстуры
 
-@pytest.fixture(scope="session")
-def browser_settings():
-    driver_options = webdriver.ChromeOptions
-    driver_options.page_load_strategy = 'normal'
-    browser.config.driver_options = driver_options
-    browser.config.window_height = 1080
-    browser.config.window_width = 1920
-    print("Браузер открыт!")
+@pytest.fixture(scope="function")
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
 
     yield
 
     print("Закрываем браузер!")
+
+
+
